6b008c1b378069aa5ca8931b466fd36661500d06
chanee13p <chanhee13p@gmail.com>
2019-09-10 18:51:33 +0900

    plot analysis

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,161 +1,161 @@
 let questions = [];
 const t_questions = [
-  ['karate', 'btw', 'single_oranges', 0.40],
-  ['dolphins', 'deg_log', 'magma', 0.40],
-  ['dolphins', 'deg_log', 'prtogn', 0.15],
+    ['karate', 'btw', 'single_oranges', 0.40],
+    ['dolphins', 'deg_log', 'magma', 0.40],
+    ['dolphins', 'deg_log', 'prtogn', 0.15],
 ];
 
 /*** TEST START : 첫 화면(로그인)에 네트워크 테스크도 함께 띄우기 ***/
 setTimeout(() => {
-  const q = t_questions[0];
-  drawGraph(q[0], q[1], q[2], q[3], 0, 'tutorial'); // data, cnet, color, span
+    const q = t_questions[0];
+    drawGraph(q[0], q[1], q[2], q[3], 0, 'tutorial'); // data, cnet, color, span
 }, 1000);
 /*** TEST END : 첫 화면(로그인)에 네트워크 테스크도 함께 띄우기 ***/
 
 const TEST_RESULT = [];
 
-function Start () {
-  changePage(0);
-  $('#startColorBlind').click(function () {
-    changePage(1);
-    $('#completeColorblindTest').click(function () {
-      blindTest();
-      changePage(2);
-      $('#startTutorial').click(function () {
-        changePage(3);
-        tutorialStart();
-      });
-    });
-  });
-}
-
-function changePage (pageNum) {
-  $('html').scrollTop(0);
-  $('body').html(Page[pageNum]);
-}
-
-function tutorialStart () {
-  setTimeout(function () {
-    tutorialTest(0);
-  }, 1000);
-}
-
-function testStart () {
-  questions = makeQuestionList();
-  console.log("questions:", questions);
-  setTimeout(function () {
-    userTest(0);
-  }, 1000);
-}
-
-function tutorialTest (testIdx) {
-  if (testIdx >= t_questions.length) {
-    changePage(4);
-    $('#startTest').click(function () {
-      changePage(5);
-      testStart(0);
-    });
-    return;
-  }
-  const q = t_questions[testIdx];
-  drawGraph(q[0], q[1], q[2], q[3], testIdx, 'tutorial'); // data, cnet, color, span
-}
-
-function userTest (testIdx) {
-  if (testIdx >= questions.length) {
-    // End of Test (48)
-    writeData();
-    changePage(6);
-    return TEST_RESULT;
-  }
-  let q = questions[testIdx];
-  drawGraph(q[0], q[1], q[2], q[3], testIdx, 'actual'); // data, cnet, color, span
-}
-
-
-function getTargetSet (nodes, centrality, spanRatio) {
-  const ret = [];
-  const min = getMinValue(nodes, centrality);
-  const max = getMaxValue(nodes, centrality);
-  const spanDistance = getSpanDistance(min, max, spanRatio);
-  const ct_distance = spanRatio > 0.2 ? 0.5 : 5.0;
-  const len = nodes.length;
-  for (let i = 0; i < len - 2; i++) {
-    for (let j = i + 1; j < len - 1; j++) {
-      const sourceMax = Math.max(nodes[i][centrality], nodes[j][centrality]);
-      const sourceMin = Math.min(nodes[i][centrality], nodes[j][centrality]);
-      const sourceDistance = sourceMax - sourceMin;
-      for (let k = i + 2; k < len; k++) {
-        const targetVal = nodes[k][centrality];
-        if (sourceMin < targetVal && targetVal < sourceMax) {
-          ret.push({
-            nodes: [i, j, k],
-            error: Math.abs(sourceDistance - spanDistance),
-            distance:
-              Math.max(sourceMax - targetVal, targetVal - sourceMin) *
-              (1 + Math.random() / ct_distance)
-          });
-        }
-      }
-    }
-  }
-  const sorted = _.sortBy(ret, ['error', 'distance']);
-  return sorted[0].nodes;
-}
-
-function getMinObj (objs, key) {
-  return _.maxBy(objs, o => -Math.abs(o[key]));
-}
-
-function getMaxObj (objs, key) {
-  return _.maxBy(objs, o => Math.abs(o[key]));
-}
-
-function getMinValue (objs, key) {
-  const maxObj = getMinObj(objs, key);
-  return maxObj[key];
-}
-
-function getMaxValue (objs, key) {
-  const maxObj = getMaxObj(objs, key);
-  return maxObj[key];
-}
-
-function getSpanDistance (min, max, span) {
-  const interval = max - min;
-  return span * interval;
-}
-function getSpanRatio (min, max, distance) {
-  return (distance - min) / (max - min);
-}
-
-function makeQuestionList () {
-  const questions = [];
-  _.forEach(Constant.dataNames, data => {
-    _.forEach(Constant.centralityNames, cent => {
-      _.forEach(Constant.colorMapNames, color => {
-        _.forEach(Constant.spans, span => {
-          questions.push([data, cent, color, span]);
+function Start() {
+    changePage(0);
+    $('#startColorBlind').click(function() {
+        changePage(1);
+        $('#completeColorblindTest').click(function() {
+            blindTest();
+            changePage(2);
+            $('#startTutorial').click(function() {
+                changePage(3);
+                tutorialStart();
+            });
         });
-      });
     });
-  });
-  return Util.shuffle(questions);
 }
 
-function blindTest () {
-  const correctVals = [12, 8, 16, 5, 3, 45];
-  let color_blind = false;
+function changePage(pageNum) {
+    $('html').scrollTop(0);
+    $('body').html(Page[pageNum]);
+}
 
-  for (let i = 1; i <= 6; i++) {
-    const val = parseInt($('#blind_test_' + i).val());
-    const correct_val = correctVals[i - 1];
-    if (val === undefined || val === '' || isNaN(val)) {
-      alert('Please enter a number in every input window.');
-      return;
+function tutorialStart() {
+    setTimeout(function() {
+        tutorialTest(0);
+    }, 1000);
+}
+
+function testStart() {
+    questions = makeQuestionList();
+    console.log("questions:", questions);
+    setTimeout(function() {
+        userTest(0);
+    }, 1000);
+}
+
+function tutorialTest(testIdx) {
+    if (testIdx >= t_questions.length) {
+        changePage(4);
+        $('#startTest').click(function() {
+            changePage(5);
+            testStart(0);
+        });
+        return;
     }
-    color_blind = val === correct_val ? color_blind : true;
-  }
-  const addedText = color_blind ? '' : 'NOT ';
-  TEST_DATA.u_colorblind = color_blind;
+    const q = t_questions[testIdx];
+    drawGraph(q[0], q[1], q[2], q[3], testIdx, 'tutorial'); // data, cnet, color, span
+}
+
+function userTest(testIdx) {
+    if (testIdx >= questions.length) {
+        // End of Test (48)
+        writeData();
+        changePage(6);
+        return TEST_RESULT;
+    }
+    let q = questions[testIdx];
+    drawGraph(q[0], q[1], q[2], q[3], testIdx, 'actual'); // data, cnet, color, span
+}
+
+
+function getTargetSet(nodes, centrality, spanRatio) {
+    const ret = [];
+    const min = getMinValue(nodes, centrality);
+    const max = getMaxValue(nodes, centrality);
+    const spanDistance = getSpanDistance(min, max, spanRatio);
+    const ct_distance = spanRatio > 0.2 ? 0.5 : 5.0;
+    const len = nodes.length;
+    for (let i = 0; i < len - 2; i++) {
+        for (let j = i + 1; j < len - 1; j++) {
+            const sourceMax = Math.max(nodes[i][centrality], nodes[j][centrality]);
+            const sourceMin = Math.min(nodes[i][centrality], nodes[j][centrality]);
+            const sourceDistance = sourceMax - sourceMin;
+            for (let k = i + 2; k < len; k++) {
+                const targetVal = nodes[k][centrality];
+                if (sourceMin < targetVal && targetVal < sourceMax) {
+                    ret.push({
+                        nodes: [i, j, k],
+                        error: Math.abs(sourceDistance - spanDistance),
+                        distance: Math.max(sourceMax - targetVal, targetVal - sourceMin) *
+                            (1 + Math.random() / ct_distance)
+                    });
+                }
+            }
+        }
+    }
+    const sorted = _.sortBy(ret, ['error', 'distance']);
+    return sorted[0].nodes;
+}
+
+function getMinObj(objs, key) {
+    return _.maxBy(objs, o => -Math.abs(o[key]));
+}
+
+function getMaxObj(objs, key) {
+    return _.maxBy(objs, o => Math.abs(o[key]));
+}
+
+function getMinValue(objs, key) {
+    const maxObj = getMinObj(objs, key);
+    return maxObj[key];
+}
+
+function getMaxValue(objs, key) {
+    const maxObj = getMaxObj(objs, key);
+    return maxObj[key];
+}
+
+function getSpanDistance(min, max, span) {
+    const interval = max - min;
+    return span * interval;
+}
+
+function getSpanRatio(min, max, distance) {
+    return (distance - min) / (max - min);
+}
+
+function makeQuestionList() {
+    const questions = [];
+    _.forEach(Constant.dataNames, data => {
+        _.forEach(Constant.centralityNames, cent => {
+            _.forEach(Constant.colorMapNames, color => {
+                _.forEach(Constant.spans, span => {
+                    questions.push([data, cent, color, span]);
+                });
+            });
+        });
+    });
+    return Util.shuffle(questions);
+}
+
+function blindTest() {
+    const correctVals = [12, 8, 16, 5, 3, 45];
+    let color_blind = false;
+
+    for (let i = 1; i <= 6; i++) {
+        const val = parseInt($('#blind_test_' + i).val());
+        const correct_val = correctVals[i - 1];
+        if (val === undefined || val === '' || isNaN(val)) {
+            alert('Please enter a number in every input window.');
+            return;
+        }
+        color_blind = val === correct_val ? color_blind : true;
+    }
+    const addedText = color_blind ? '' : 'NOT ';
+    TEST_DATA.u_colorblind = color_blind;
 }
--- javascripts/statistics.js
+++ javascripts/statistics.js
@@ -1,29 +1,97 @@
 main();
 
-async function main () {
-  const datanames = ['lesmis', 'football', 'jazz'];
-  const centralities = ['page', 'random'];
-  const colormaps = ['blue', 'RdYlBu', 'jet', 'viridis'];
-  const spans = [0.15, 0.40];
-  const valueArea = [0, 1, 2];
+async function main() {
+    const svg = d3.select('svg#statistics');
 
-  const dataset = await getDataset();
-  const conditions = getConditionList(datanames, centralities, colormaps, spans, valueArea)
-  console.log(dataset);
+    const datanames = ['lesmis', 'football', 'jazz'];
+    const centralities = ['page', 'random'];
+    const colormaps = ['blue', 'RdYlBu', 'jet', 'viridis'];
+    const spans = [0.15, 0.40];
+    const valueArea = [0, 1, 2];
 
-  _.forEach(conditions, condition => {
-    console.log(condition[0].value, condition[1].value, condition[2].value, condition[3].value, condition[4].value);
-    const filltered = getConditioned(dataset, condition, 'nodeValue');
-    console.log(filltered);
-    const bootstraped = bootstrap(filltered, 1000);
-    // console.log(bootstraped)
+    const dataset = await getDataset();
+    const conditions = getConditionList(datanames, centralities, colormaps, spans);
 
-    //     // 색, 데이터, 중심성, 스팬 별로 데이터 필터링 완료
-    //     // TODO: 값이 높고 낮은 정도에 따라 나누기 : Problem - 값이 대부분 비슷해서 구간을 나눌 수 없다.
-    //     // TODO: 각각 평균과 표준편차 구하기
-    //     // TODO: 그리기
-  })
+    _.forEach(conditions, (condition, c_idx) => {
+        const filltered = getConditioned(dataset, condition, 'nodeValue', 'time');
+        drawInstances(svg, c_idx, condition, filltered);
+        const bootstraped = bootstrap(filltered, 1000);
+        //     // TODO: 값이 높고 낮은 정도에 따라 나누기 : Problem - 값이 대부분 비슷해서 구간을 나눌 수 없다.
+        //     // TODO: 각각 평균과 표준편차 구하기
+        //     // TODO: 그리기
+    })
 
+}
+
+function drawInstances(svg, idx, condition, instances) {
+    const w = 250;
+    const h = 100;
+    const margin = 100;
+    svg.append('line').attrs({
+        x1: margin,
+        x2: margin + w,
+        y1: idx * (h + margin) + h,
+        y2: idx * (h + margin) + h,
+        stroke: '#555'
+    });
+    svg.append('line').attrs({
+        x1: margin,
+        x2: margin,
+        y1: idx * (h + margin),
+        y2: idx * (h + margin) + h,
+        stroke: '#555'
+    });
+    svg.append('line').attrs({
+        x1: 0,
+        x2: margin + w * 2,
+        y1: idx * (h + margin) + h + margin * 3 / 4,
+        y2: idx * (h + margin) + h + margin * 3 / 4,
+        stroke: '#aaa'
+    });
+    svg.append('text').text(`${condition[0].value},  ${condition[1].value},  ${condition[2].value},  ${condition[3].value}`).attrs({
+        x: margin,
+        y: idx * (h + margin) + h + margin / 2,
+    });
+    svg.append('text').text('value').attrs({
+        x: margin + w - 20,
+        y: idx * (h + margin) + h + 40,
+    });
+    svg.append('text').text('time').attrs({
+        x: margin - 60,
+        y: idx * (h + margin) + 15,
+    });
+
+    const time = [0, 2.5, 5, 7.5, 10, 12.5, 15, 17.5, 20, 22.5, 25, 27.5, 30];
+    const value = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, '', ''];
+    const zero_x = margin;
+    const zero_y = idx * (h + margin) + h;
+    const valueR = 250;
+    const timeR = 3;
+
+    for (let i = 0; i <= 12; i += 2) {
+        let x = zero_x + value[i] * valueR;
+        let y = zero_y - time[i] * timeR;
+        svg.append('text').text(value[i]).attrs({
+            x: x,
+            y: zero_y + 20,
+        });
+        svg.append('text').text(time[i]).attrs({
+            x: zero_x - 20,
+            y: y,
+        });
+    }
+    for (let i = 0; i < instances.length; i++) {
+        let x = zero_x + instances[i][0] * valueR;
+        let y = zero_y - instances[i][1] * timeR;
+        if (instances[i][1] > 30) continue;
+        svg.append('circle').attrs({
+            cx: x,
+            cy: y,
+            r: 3,
+            fill: '#052',
+            opacity: 0.2
+        });
+    }
 }
 
 /**
@@ -32,15 +100,15 @@ async function main () {
  * @param {Array} data 
  * @param {number} numOfSamples 
  */
-function bootstrap (data, numOfSamples) {
-  const numOfInstances = data.length;
-  const bootstrapedSamples = []
-  for (let i = 0; i < numOfSamples; i++) {
-    const randomIdx = getRandomInteger(0, numOfInstances - 1);
-    const instance = data[randomIdx];
-    bootstrapedSamples.push(instance);
-  }
-  return bootstrapedSamples;
+function bootstrap(data, numOfSamples) {
+    const numOfInstances = data.length;
+    const bootstrapedSamples = []
+    for (let i = 0; i < numOfSamples; i++) {
+        const randomIdx = getRandomInteger(0, numOfInstances - 1);
+        const instance = data[randomIdx];
+        bootstrapedSamples.push(instance);
+    }
+    return bootstrapedSamples;
 }
 
 /**
@@ -48,50 +116,50 @@ function bootstrap (data, numOfSamples) {
  * @param {number} min The minimum integer of random number
  * @param {number} max The maximum integer of random number
  */
-function getRandomInteger (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomInteger(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // test function
-function testBootstrap () {
-  data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-  cnt = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-  for (let i = 0; i < 1000; i++) {
-    const bt = bootstrap(data, 100);
-    for (let sample of bt) {
-      cnt[sample] += 1;
+function testBootstrap() {
+    data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+    cnt = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for (let i = 0; i < 1000; i++) {
+        const bt = bootstrap(data, 100);
+        for (let sample of bt) {
+            cnt[sample] += 1;
+        }
     }
-  }
-  console.log(cnt);
+    console.log(cnt);
 }
 
-async function getDataset () {
-  return await d3.csv("./data/test_colormap(1).csv", d => {
-    const leftCls = Math.abs(d['TargetNode_value'] - d['ComparisonNode_left']);
-    const rightCls = Math.abs(d['TargetNode_value'] - d['ComparisonNode_right']);
-    const correctNodeValue = leftCls < rightCls ? d['ComparisonNode_left'] : d['ComparisonNode_right'];
-    let valueArea = Math.floor(correctNodeValue * 3);
-    valueArea = valueArea < 0 ? 0 : valueArea;
-    valueArea = valueArea > 2 ? 2 : valueArea;
-    const colormapNames = {
-      'single_blue': 'blue',
-      'viridis': 'viridis',
-      'jet': 'jet',
-      'red_blue': 'RdYlBu'
-    }
-    return {
-      dataname: d['data'],
-      centrality: d['NodeAttributes'],
-      nodeValue: correctNodeValue,
-      colormap: colormapNames[d['colormap']],
-      span: d['span'] * 1,
-      valueArea: valueArea,
-      time: d['time'] * 1,
-      correctness: d['isCorrect'],
-    }
-  });
+async function getDataset() {
+    return await d3.csv("./data/test_colormap(1).csv", d => {
+        const leftCls = Math.abs(d['TargetNode_value'] - d['ComparisonNode_left']);
+        const rightCls = Math.abs(d['TargetNode_value'] - d['ComparisonNode_right']);
+        const correctNodeValue = leftCls < rightCls ? d['ComparisonNode_left'] : d['ComparisonNode_right'];
+        let valueArea = Math.floor(correctNodeValue * 3);
+        valueArea = valueArea < 0 ? 0 : valueArea;
+        valueArea = valueArea > 2 ? 2 : valueArea;
+        const colormapNames = {
+            'single_blue': 'blue',
+            'viridis': 'viridis',
+            'jet': 'jet',
+            'red_blue': 'RdYlBu'
+        }
+        return {
+            dataname: d['data'],
+            centrality: d['NodeAttributes'],
+            nodeValue: correctNodeValue,
+            colormap: colormapNames[d['colormap']],
+            span: d['span'] * 1,
+            valueArea: valueArea,
+            time: d['time'] * 1,
+            correctness: d['isCorrect'],
+        }
+    });
 }
 
 /**
@@ -108,18 +176,18 @@ async function getDataset () {
  * getConditioned(array, conditions, key) 
  * // It returns [26, 24] 
  */
-function getConditioned (arr, conditions, key) {
-  const ret = []
-  for (let i = 0; i < arr.length; i++) {
-    const elem = arr[i];
-    let cnt = 0;
-    for (let j = 0; j < conditions.length; j++) {
-      const cond = conditions[j];
-      if (elem[cond.key] == cond.value) cnt++;
+function getConditioned(arr, conditions, key1, key2) {
+    const ret = []
+    for (let i = 0; i < arr.length; i++) {
+        const elem = arr[i];
+        let cnt = 0;
+        for (let j = 0; j < conditions.length; j++) {
+            const cond = conditions[j];
+            if (elem[cond.key] == cond.value) cnt++;
+        }
+        if (cnt === conditions.length) ret.push([elem[key1], elem[key2]]);
     }
-    if (cnt === conditions.length) ret.push(elem[key]);
-  }
-  return ret;
+    return ret;
 }
 
 /**
@@ -129,24 +197,21 @@ function getConditioned (arr, conditions, key) {
  * @param {*} colormaps
  * @param {*} spans 
  */
-function getConditionList (datanames, centralities, colormaps, spans, valueAreas) {
-  const conditions = [];
-  _.forEach(datanames, dataname => {
-    _.forEach(centralities, cent => {
-      _.forEach(colormaps, colormap => {
-        _.forEach(spans, span => {
-          _.forEach(valueAreas, a => {
-            conditions.push([
-              { key: 'dataname', value: dataname },
-              { key: 'centrality', value: cent },
-              { key: 'colormap', value: colormap },
-              { key: 'span', value: span },
-              { key: 'valueArea', value: a }
-            ]);
-          })
-        })
-      })
+function getConditionList(datanames, centralities, colormaps, spans, ) {
+    const conditions = [];
+    _.forEach(datanames, dataname => {
+        _.forEach(centralities, cent => {
+            _.forEach(colormaps, colormap => {
+                _.forEach(spans, span => {
+                    conditions.push([
+                        { key: 'dataname', value: dataname },
+                        { key: 'centrality', value: cent },
+                        { key: 'colormap', value: colormap },
+                        { key: 'span', value: span },
+                    ]);
+                })
+            })
+        });
     });
-  });
-  return conditions;
+    return conditions;
 }
